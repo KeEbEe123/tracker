d506d5a93ac0aca5b4072c41aeedf9f3a3094005
KeEbEe123 <keertan.k@gmail.com>
2025-05-06 21:27:39 +0530

    added patch notes

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import { Navbar } from "@/components/navbar";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { AuthProvider } from "@/providers/auth-provider";
 import { Toaster } from "sonner";
+import PatchNotesPopup from "@/components/PatchNotesPopup";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({ children }) {
               <Navbar />
               <main className="pt-24">
                 {children}
+                <PatchNotesPopup />
                 <Toaster position="top-right" />
               </main>
             </div>
